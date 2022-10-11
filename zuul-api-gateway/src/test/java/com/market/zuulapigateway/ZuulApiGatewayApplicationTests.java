12963a914f9793ba7db17f701c91a45b231454aa
Syamkumar08 <syamramanatham8@gmail.com>
2022-10-11 15:31:59 +0530

    backend changes

---
+++ zuul-api-gateway/src/test/java/com/market/zuulapigateway/ZuulApiGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.market.zuulapigateway;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class ZuulApiGatewayApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
